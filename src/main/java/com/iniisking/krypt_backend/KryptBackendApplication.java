5c91c0e8ba18631ce0bd4adfab5f22498901c4dd
IniOluwa Longe <inilonge@gmail.com>
2025-08-22 02:08:12 +0100

    Auth setup

---
+++ src/main/java/com/iniisking/krypt_backend/KryptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iniisking.krypt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KryptBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iniisking/krypt_backend/KryptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniisking.krypt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KryptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
